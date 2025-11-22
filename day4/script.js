01c6525d4f24dafec156787a30ac74abf5e4d078
suraj05112 <yadavgsuraj2005@gmail.com>
2025-11-22 23:56:45 +0530

    suraj

---
+++ day4/script.js
@@ -0,0 +1,9 @@
+// Input method
+
+console.log("Welcome to FSD Class!");
+document.write("Welcome to FSD Class");
+document.writeln("Welcome to FSD Class line!")
+alert("Welcome to FSD Class");
+console.error("Error message");
+
+// Input
