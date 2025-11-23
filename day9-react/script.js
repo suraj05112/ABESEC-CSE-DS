91446abb6f513de9ce6c71bbab8a4d3e5b0e813a
suraj05112 <yadavgsuraj2005@gmail.com>
2025-11-23 15:16:37 +0530

    basic React

---
+++ day9-react/script.js
@@ -0,0 +1,22 @@
+{ 
+    <div id="outer-div">
+<h1 id="inner-div"></h1>
+    <h1 id="heading"></h1>
+<h2 id="sub-heading"></h2>
+<h3 id="third-heading"></h3>
+</div>
+const heading = React.createElement("div", { id: "outer-div" },
+    React.createElement("div", { id: "inner-div" },
+        React.createElement("h1", { id: "heading1" }, "Hello! React Learners"),
+        React.createElement("h1", { id: "heading2" }, "Hello! React Learners"),
+        React.createElement("h1", { id: "heading3" }, "Hello! React Learners"),
+        React.createElement("h1", { id: "heading4" }, "Hello! React Learners")
+    )
+);
+
+console.log(heading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
+} ;
